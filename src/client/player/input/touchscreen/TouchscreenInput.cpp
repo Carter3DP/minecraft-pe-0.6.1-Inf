ba70106b6b36524e74e72e2b0be5b591998b8879
Carter3DP <ccwallace03@outlook.com>
2026-06-24 23:22:09 -0400

    Center pause and chat
    Only for touchscreen devices

--- src/client/player/input/touchscreen/TouchscreenInput.cpp
+++ src/client/player/input/touchscreen/TouchscreenInput.cpp
@@ -165,8 +165,8 @@ void TouchscreenInput_TestFps::onConfigChanged(const Config& c) {
     float maxPixels = _minecraft->pixelCalc.millimetersToPixels(10);
     // float btnSize = Mth::Min(18 * Gui::GuiScale, maxPixels);
 	float btnSize = pc.millimetersToPixels(18 * Gui::GuiScale);
-	_model.addArea(AREA_PAUSE, aPause = new RectangleArea(w - 4 - btnSize, 4, w - 4, 4 + btnSize));
-	_model.addArea(AREA_CHAT,  aChat  = new RectangleArea(w - 8 - btnSize * 2, 4, w - 8 - btnSize, 4 + btnSize));
+	_model.addArea(AREA_PAUSE, aPause = new RectangleArea((w / 2) - btnSize, 4, w / 2, 4 + btnSize));
+	_model.addArea(AREA_CHAT,  aChat  = new RectangleArea((w / 2) - 4 - (btnSize * 2), 4, (w / 2) - 4 - btnSize, 4 + btnSize));
 
 	//rebuild();
 }
